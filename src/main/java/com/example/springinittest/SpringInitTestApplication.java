f790a622c09b214d0b31c7cb3fbbeb0fa046c6b0
jeremiahvaughan <jeremiah.t.vaughan@gmail.com>
2020-12-21 11:44:45 -0500

    init

---
+++ src/main/java/com/example/springinittest/SpringInitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInitTestApplication.class, args);
+    }
+
+}
